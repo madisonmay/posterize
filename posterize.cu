12408a06673267f98bc6e11084c50b0a42e7adf7
benkahle <ben@kahle.org>
2014-04-27 20:31:06 -0400

    Added channel variable use to smooth dummy and removed unnecessary use of cudaDeviceSynchronize()

--- posterize.cu
+++ posterize.cu
@@ -36,11 +36,10 @@ void smooth(const unsigned char* input,
       return;
   }
   int idx = y*cols+x;
-  int w = 256/n;
-
-  output[idx*3+0] = input[idx*3+0];
-  output[idx*3+1] = input[idx*3+1];
-  output[idx*3+2] = input[idx*3+2];
+  //int w = 256/n;
+  output[idx*channels+0] = input[idx*channels+0];
+  output[idx*channels+1] = input[idx*channels+1];
+  output[idx*channels+2] = input[idx*channels+2];
 }
 
 char* process(char* image_rgb, size_t cols, size_t rows, int channels, int colors)
@@ -58,9 +57,7 @@ char* process(char* image_rgb, size_t cols, size_t rows, int channels, int color
   const dim3 blockSize(8,8,1);
   const dim3 gridSize(cols/blockSize.x+1,rows/blockSize.y+1,1);
   posterize<<<gridSize, blockSize>>>(d_img_in, d_img_out, cols, rows, channels, colors);
-  gpuErrchk(cudaDeviceSynchronize());
   smooth<<<gridSize, blockSize>>>(d_img_out, d_smooth_out, cols, rows, channels, colors);
-  gpuErrchk(cudaDeviceSynchronize());
   gpuErrchk(cudaMemcpy(h_img_out, d_smooth_out, image_data_size, cudaMemcpyDeviceToHost));
   return h_img_out;
 }
