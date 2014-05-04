936bd0a3a6d741629d77e4f030d5f82fe8638359
Madison May <madison.may@students.olin.edu>
2014-05-04 02:48:18 -0400

    Optimization -- no longer using a running mode operation

--- posterize.h
+++ posterize.h
@@ -3,14 +3,15 @@
 #include <stdio.h>
 
 __global__
-void mode_kernel(const unsigned char* const input, 
-          unsigned char* const output, 
-          size_t cols, size_t rows, int channels);
+void mode_kernel(const unsigned char* input, 
+                 unsigned char* output, 
+                 size_t cols, size_t rows, int channels,
+                 int colors, unsigned int* hist);
 
 __global__
-void reduce_kernel(const unsigned char* const input, 
-            unsigned char* const output, 
-            size_t cols, size_t rows, int channels, int n);
+void reduce_kernel(const unsigned char* input, 
+                   unsigned char* output, 
+                   size_t cols, size_t rows, int channels, int n);
 
 char* processPosterize(char* image_rgb, size_t cols, size_t rows, int channels, int colors);
 #endif
