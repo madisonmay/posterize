11c0b8224353dddb914d9cfde1e824e49d7c24a4
Madison May <madison.may@students.olin.edu>
2014-05-03 20:42:19 -0400

    Posterization success.

--- posterize.cu
+++ posterize.cu
@@ -38,9 +38,9 @@ void mode(const unsigned char* input,
 
   int idx = y*cols+x;
 
-  int dim = 9;
+  int dim = 11;
 
-  int offset, Offset = 0;
+  int offset, Offset;
 
   int count = 0, maxCount = 0;
 
@@ -52,8 +52,8 @@ void mode(const unsigned char* input,
   for (i = 0; i < channels; i++) {
     // for every pixel per channel...
     for (j = -dim/2; j <= dim/2; j++){
-      count = 0;
       for (k = -dim/2; k <= dim/2; k++) {
+        count = 0;
         offset = idx+cols*j+k;
         if ((x+k >= cols || y+j >= rows) || (x+k < 0 || y+j < 0)) {
           continue;
@@ -63,6 +63,7 @@ void mode(const unsigned char* input,
         for (J = -dim/2; J <= dim/2; J++){
           for (K = -dim/2; K <= dim/2; K++) {
             Offset = idx+cols*J+K;
+
             if ((x+K >= cols || y+J >= rows) || (x+K < 0 || y+J < 0)) {
               continue;
             }
@@ -72,6 +73,7 @@ void mode(const unsigned char* input,
             }
           }
         }
+
         if (count > maxCount) {
           maxCount = count;
           mode = input[offset*channels + i];
@@ -153,18 +155,22 @@ void smooth(const unsigned char* input,
 char* process(char* image_rgb, size_t cols, size_t rows, int channels, int colors)
 {
   unsigned char *d_img_in;
-  unsigned char *d_img_out;
+  unsigned char *d_img_reduce;
+  unsigned char *d_img_mode;
   char *h_img_out;
   size_t image_data_size = sizeof(unsigned char)*cols*rows*channels;
   h_img_out = (char *)malloc(image_data_size);
   gpuErrchk(cudaMalloc(&d_img_in, image_data_size));
-  gpuErrchk(cudaMalloc(&d_img_out, image_data_size));
+  gpuErrchk(cudaMalloc(&d_img_reduce, image_data_size));
   gpuErrchk(cudaMemcpy(d_img_in, image_rgb, image_data_size, cudaMemcpyHostToDevice));
   const dim3 blockSize(16,16,1);
   const dim3 gridSize(cols/blockSize.x+1,rows/blockSize.y+1,1);
-  // posterize<<<gridSize, blockSize>>>(d_img_in, d_img_out, cols, rows, channels, colors);
-  mode<<<gridSize, blockSize>>>(d_img_in, d_img_out, cols, rows, channels);
+  posterize<<<gridSize, blockSize>>>(d_img_in, d_img_reduce, cols, rows, channels, colors);
   gpuErrchk(cudaFree(d_img_in));
-  gpuErrchk(cudaMemcpy(h_img_out, d_img_out, image_data_size, cudaMemcpyDeviceToHost));
+  gpuErrchk(cudaMalloc(&d_img_mode, image_data_size));
+  mode<<<gridSize, blockSize>>>(d_img_reduce, d_img_mode, cols, rows, channels);
+  gpuErrchk(cudaFree(d_img_reduce));
+  gpuErrchk(cudaMemcpy(h_img_out, d_img_mode, image_data_size, cudaMemcpyDeviceToHost));
+  gpuErrchk(cudaFree(d_img_mode));
   return h_img_out;
 }
